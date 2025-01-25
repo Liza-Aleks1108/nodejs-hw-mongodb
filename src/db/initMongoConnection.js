a9fdc94926a49bf6ad27c51ede48c7b9af14f3a1
Алексєєнко Єлизавета <lizaaleks2005@gmail.com>
2025-01-25 22:09:27 +0000

    Update initMongoConnection.js

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,5 +1,26 @@
 // src/db/initMongoConnection.js
 
+// import mongoose from 'mongoose';
+
+// import { env } from '../utils/env.js';
+
+// export const initMongoConnection = async () => {
+//   try {
+//     const user = env('MONGODB_USER');
+//     const pwd = env('MONGODB_PASSWORD');
+//     const url = env('MONGODB_URL');
+//     const db = env('MONGODB_DB');
+
+//     await mongoose.connect(
+//       `mongodb+srv://${user}:${pwd}@${url}/${db}/?retryWrites=true&w=majority&appName=mongodb`,
+//     );
+//     console.log('Mongo connection successfully established!');
+//   } catch (e) {
+//     console.log('Error while setting up mongo connection', e);
+//     throw e;
+//   }
+// };
+
 import mongoose from 'mongoose';
 
 import { env } from '../utils/env.js';
@@ -9,10 +30,10 @@ export const initMongoConnection = async () => {
     const user = env('MONGODB_USER');
     const pwd = env('MONGODB_PASSWORD');
     const url = env('MONGODB_URL');
-    const db = env('MONGODB_DB');
+    // const db = env('MONGODB_DB');
 
     await mongoose.connect(
-      `mongodb+srv://${user}:${pwd}@${url}/${db}/?retryWrites=true&w=majority&appName=mongodb`,
+      `mongodb+srv://${user}:${pwd}@${url}/?retryWrites=true&w=majority&appName=mongodb`,
     );
     console.log('Mongo connection successfully established!');
   } catch (e) {
