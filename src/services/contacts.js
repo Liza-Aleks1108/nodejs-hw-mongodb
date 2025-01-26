ee7fdb0ae08e7a427268333f406e75eaf8c544bf
Алексєєнко Єлизавета <lizaaleks2005@gmail.com>
2025-01-26 15:15:57 +0000

    pagination

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,13 +1,34 @@
 // src/services/contacts.js
 
 import { ContactsCollection } from '../db/models/contacts.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = async () => {
-  try {
-    return await ContactsCollection.find();
-  } catch (error) {
-    throw new Error('Error fetching contacts: ' + error.message);
-  }
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = 'asc',
+  sortBy = '_id',
+}) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = ContactsCollection.find();
+
+  const [contactsCount, contacts] = await Promise.all([
+    ContactsCollection.find().merge(contactsQuery).countDocuments(),
+    contactsQuery
+      .skip(skip)
+      .limit(limit)
+      .sort({ [sortBy]: sortOrder })
+      .exec(),
+  ]);
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 export const getContactById = async (id) => {
