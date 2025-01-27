c0a2c90a8d36f1accba821bb33b80a2043b0d884
Алексєєнко Єлизавета <lizaaleks2005@gmail.com>
2025-01-27 11:07:47 +0000

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -31,56 +31,38 @@ export const getAllContacts = async ({
   };
 };
 
-export const getContactById = async (id) => {
-  try {
-    const contact = await ContactsCollection.findById(id);
-    if (!contact) {
-      throw new Error('Contact not found');
-    }
-    return contact;
-  } catch (error) {
-    throw new Error(error.message);
-  }
+export const getContactById = async ({ _id, userId }) => {
+  return await ContactsCollection.findOne({ _id, userId });
 };
 
 export const createContact = async (payload) => {
-  try {
-    return await ContactsCollection.create(payload);
-  } catch (error) {
-    throw new Error('Error creating contact: ' + error.message);
-  }
+  return await ContactsCollection.create(payload);
 };
 
-export const updateContact = async (contactId, payload, options = {}) => {
-  try {
-    const updatedContact = await ContactsCollection.findByIdAndUpdate(
-      { _id: contactId },
-      payload,
-      {
-        new: true,
-        includeResultMetadata: true,
-        ...options,
-      },
-    );
-    if (!updatedContact) {
-      throw new Error('Contact not found');
-    }
-    return updatedContact;
-  } catch (error) {
-    throw new Error('Error updating contact: ' + error.message);
-  }
+export const updateContact = async (
+  { contactId, userId },
+  payload,
+  options = {},
+) => {
+  const contactToUpdate = await ContactsCollection.findOneAndUpdate(
+    {
+      _id: contactId,
+      userId,
+    },
+    payload,
+    {
+      new: true,
+      ...options,
+    },
+  );
+
+  if (!contactToUpdate) return null;
+  return contactToUpdate;
 };
 
-export const deleteContact = async (contactId) => {
-  try {
-    const deletedContact = await ContactsCollection.findByIdAndDelete(
-      contactId,
-    );
-    if (!deletedContact) {
-      throw new Error('Contact not found');
-    }
-    return deletedContact;
-  } catch (error) {
-    throw new Error('Error deleting contact: ' + error.message);
-  }
+export const deleteContact = async ({ contactId, userId }) => {
+  return await ContactsCollection.findOneAndDelete({
+    _id: contactId,
+    userId,
+  });
 };
